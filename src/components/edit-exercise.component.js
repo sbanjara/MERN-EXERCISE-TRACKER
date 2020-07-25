7397c7941798d4f14f46934882412993992d1301
Sabin Banjara <sbanjara@stu.jsu.edu>
2020-07-25 01:45:42 -0500

    Front-end Logic

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class EditExercise extends Component {
+
+    render() {
+        return (
+
+           <div>
+                <p>You are on the Edit Exercises component!!</p>
+           </div>
+
+        );
+    }
+}
---
+++ src/components/exercise-list.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class ExerciseList extends Component {
+
+    render() {
+        return (
+
+           <div>
+                <p>You are on the Exercises List component!!</p>
+           </div>
+
+        );
+    }
+}
